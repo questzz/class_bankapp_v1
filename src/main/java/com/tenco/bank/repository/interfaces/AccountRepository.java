7406915ab17bbd11dc762e0a3e73c886c1ece122
questzz <questzz@naver.com>
2023-04-17 12:30:54 +0900

    mybatis 설정 및 기본 화면 구성 1

---
+++ src/main/java/com/tenco/bank/repository/interfaces/AccountRepository.java
@@ -0,0 +1,22 @@
+package com.tenco.bank.repository.interfaces;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tenco.bank.repository.model.Account;
+
+@Mapper // mybatis 연결 처리 
+public interface AccountRepository {
+	
+	public int insert(Account account); 
+	public int updateById(Account account); 
+	public int deleteById(int id); 
+	
+	public List<Account> findAll();   
+	public Account findById(int id); 
+}
+
+
+
+
